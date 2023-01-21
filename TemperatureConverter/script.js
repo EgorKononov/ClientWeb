25c8b538468a4ab569174ab1e55a7986be3bcb6a
EgorKononov <egor.academ@gmail.com>
2023-01-21 21:38:50 +0700

    Исправлена задача "Перевод температуры"

--- TemperatureConverter/script.js
+++ TemperatureConverter/script.js
@@ -1,16 +1,20 @@
-document.addEventListener("DOMContentLoaded", function (){
+document.addEventListener("DOMContentLoaded", function () {
     var form = document.getElementById("form");
     var celsiusTemperatureField = document.getElementById("celsius-temperature-field");
     var convertButton = document.getElementById("convert-button");
     var kelvinTemperatureLabel = document.getElementById("kelvin-temperature-label");
     var fahrenheitTemperatureLabel = document.getElementById("fahrenheit-temperature-label");
 
-    form.addEventListener("submit", function (e){
+    form.addEventListener("submit", function (e) {
         e.preventDefault();
     });
 
-    convertButton.addEventListener("click", function (){
-        kelvinTemperatureLabel.textContent = (Number(celsiusTemperatureField.value) + 273.15).toString();
-        fahrenheitTemperatureLabel.textContent = (Number(celsiusTemperatureField.value) * 1.8 + 32).toString();
+    convertButton.addEventListener("click", function () {
+        var celsiusTemperatureFieldValue = celsiusTemperatureField.value.trim();
+
+        if (celsiusTemperatureFieldValue.length !== 0) {
+            kelvinTemperatureLabel.textContent = (Number(celsiusTemperatureFieldValue) + 273.15).toString();
+            fahrenheitTemperatureLabel.textContent = (Number(celsiusTemperatureFieldValue) * 1.8 + 32).toString();
+        }
     });
 });
