ae33740808337c4e75d69e24770a08f3aca26bc1
EgorKononov <egor.academ@gmail.com>
2023-01-22 21:37:23 +0700

    Исправлена задача "Todo List"

--- TodoList/script.js
+++ TodoList/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let form = document.getElementById("form");
+    var form = document.getElementById("form");
     var newTodoField = document.getElementById("new-todo-field");
     var addButton = document.getElementById("add-button");
     var todoList = document.getElementById("todo-list");
@@ -10,23 +10,59 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     addButton.addEventListener("click", function () {
-        var newTodoFieldValue = newTodoField.value;
+        var newTodoFieldValue = newTodoField.value.trim();
 
-        if (newTodoFieldValue.trim().length !== 0) {
+        if (newTodoFieldValue.length === 0) {
+            errorMessage.style.display = "block";
+            newTodoField.classList.add("invalid");
+        } else {
             errorMessage.style.display = "none";
+            newTodoField.classList.remove("invalid");
             var todoItem = document.createElement("li");
             todoItem.classList.add("margin-vertical");
-            todoItem.innerHTML = "<span class='margin-horizontal'></span><button type='button'>Удалить</button>";
-            todoItem.firstChild.textContent = newTodoFieldValue;
-
-            todoItem.lastChild.addEventListener("click", function () {
-                todoItem.remove();
-            })
-
+            setViewMode();
             todoList.appendChild(todoItem);
             newTodoField.value = "";
-        } else {
-            errorMessage.style.display = "block";
+        }
+
+        function setViewMode() {
+            todoItem.innerHTML = "<div class='todo-item-block'></div>" +
+                "<button type='button' class='edit-button'>Редактировать</button>" +
+                "<button type='button' class='delete-button'>Удалить</button>";
+            todoItem.querySelector(".todo-item-block").textContent = newTodoFieldValue;
+
+            todoItem.querySelector(".edit-button").addEventListener("click", function () {
+                setEditMode();
+            });
+
+            todoItem.querySelector(".delete-button").addEventListener("click", function () {
+                todoItem.remove();
+            });
+        }
+
+        function setEditMode() {
+            todoItem.innerHTML = "<input class='edit-todo-item' type='text'>" +
+                "<button type='button' class='save-button'>Сохранить</button>" +
+                "<button type='button' class='cancel-button'>Отмена</button>";
+
+            var editedTodoField = todoItem.querySelector(".edit-todo-item");
+            editedTodoField.value = newTodoFieldValue;
+
+            todoItem.querySelector(".save-button").addEventListener("click", function () {
+                var editedTodoFieldValue = editedTodoField.value.trim();
+
+                if (editedTodoFieldValue.length === 0) {
+                    editedTodoField.classList.add("invalid");
+                } else {
+                    editedTodoField.classList.remove("invalid");
+                    newTodoFieldValue = editedTodoFieldValue;
+                    setViewMode();
+                }
+            });
+
+            todoItem.querySelector(".cancel-button").addEventListener("click", function () {
+                setViewMode();
+            });
         }
     });
 });
